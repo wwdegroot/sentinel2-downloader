44a232e0f50203b7e9ff149fd8fc64b5d3910d8e
wwdegroot <wouter.w.de.groot@gmail.com>
2023-11-18 19:43:47 +0100

    refactoring of preview return dict

--- st2dl/preview.py
+++ st2dl/preview.py
@@ -4,9 +4,11 @@ from typing import List
 def get_preview_download_links(search_json) -> List[dict[str, str]]:
     return [
         {
+            "id": str(i),
+            "productid": v["Id"],
             "url": v["Assets"][0]["DownloadLink"],
             "origindate": v["OriginDate"],
             "name": v["Name"],
         }
-        for v in search_json["value"]
+        for i, v in enumerate(search_json["value"])
     ]
