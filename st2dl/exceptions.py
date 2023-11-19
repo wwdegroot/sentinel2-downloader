8f640af100c6e48de351bbe9ab8d802af7a49477
wwdegroot <wouter.w.de.groot@gmail.com>
2023-11-19 16:16:06 +0100

    exception for bad search request

--- st2dl/exceptions.py
+++ st2dl/exceptions.py
@@ -2,3 +2,9 @@ class InvalidWktPointArgument(Exception):
     """Raised when the WKT string is not a valid point"""
 
     pass
+
+
+class SearchException(Exception):
+    """Raised when search endpoint returned a non 200 statuscode"""
+
+    pass
