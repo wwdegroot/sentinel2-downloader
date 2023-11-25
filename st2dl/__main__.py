91a7285d8973338bc697d848bc2bf37416fcd4bf
wwdegroot <wouter.w.de.groot@gmail.com>
2023-11-25 14:01:26 +0100

    async main with refactored search data function

--- st2dl/__main__.py
+++ st2dl/__main__.py
@@ -1,15 +1,14 @@
-import sys
+import msgspec
 from dotenv import load_dotenv
-import httpx
-import orjson
+import asyncio
 from rich import print
-import click
+import asyncclick as click
 from rich.prompt import Prompt
 from st2dl.auth import get_access_token
 from st2dl.download.preview import get_preview_download_links
 from st2dl.download.product import download_products_data
 from st2dl.cli import wkt_to_point, show_preview_urls, daterange_to_timestamp
-from st2dl.download.search import ESA_SEARCH_URL
+from st2dl.download.search import search_odata
 
 
 @click.command()
@@ -49,7 +48,7 @@ from st2dl.download.search import ESA_SEARCH_URL
     show_default=True,
     help="Debug the http requests and extra debug logging",
 )
-def main(
+async def main(
     pointofinterest: str,
     daterange: str,
     max_: int,
@@ -66,6 +65,9 @@ def main(
     :param pointofinterest:
     :param max_:
     :param cloud_coverage:
+    :param username:
+    :param password:
+    :param debug:
     :return:
     """
 
@@ -74,24 +76,11 @@ def main(
     print(f"coordinates: lat: {lat:.4f}, long: {long:.4f}")
     print(f"maximum results: {max_}")
     print(f"cloud coverage percentage less then: {cloud_coverage:.2f}")
-    # filter voor zoeken op cloudCover, Productype en orbitDirection.
-    # lt = less then
-    # eq = equal to
-    # gt = greater then
-    search_filter = f"OData.CSC.Intersects(area=geography'SRID=4326;POINT ({long:.4f} {lat:.4f})') and Attributes/OData.CSC.DoubleAttribute/any(att:att/Name eq 'cloudCover' and att/OData.CSC.DoubleAttribute/Value lt {cloud_coverage:.2f}) and Attributes/OData.CSC.StringAttribute/any(att:att/Name eq 'productType' and att/OData.CSC.StringAttribute/Value eq 'S2MSI2A') and ContentDate/Start gt {time_gt} and ContentDate/Start lt {time_lt}"
-    with httpx.Client() as client:
-        r = client.get(
-            url=f"{ESA_SEARCH_URL}?$filter={search_filter}&$top={max_}&$expand=Assets",
-            timeout=60,
-        )
-        if not r.status_code == 200:
-            print(f"Error getting data: {r.text}")
-            sys.exit(-1)
-        search_data = r.json()
-        if debug:
-            with open("../search_data.json", "wb") as f:
-                f.write(orjson.dumps(search_data))
 
+    search_data = await search_odata(long, lat, cloud_coverage, time_lt, time_gt, max_)
+    if debug:
+        with open("../search_data.json", "wb") as f:
+            f.write(msgspec.json.encode(search_data))
     preview_urls = get_preview_download_links(search_data)
     show_preview_urls(preview_urls)
     prompt = Prompt()
@@ -111,4 +100,4 @@ def main(
 
 if __name__ == "__main__":
     load_dotenv()
-    main()
+    asyncio.run(main())
